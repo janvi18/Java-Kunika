60645f6a102909ca43c7ba79884c16c318c59585
janvi18 <sjanvi9333@gmail.com>
2023-02-06 15:48:22 +0530

    first

---
+++ add.java
@@ -0,0 +1,18 @@
+public class add {
+
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    int sub = a - b;
+    int mul = a * b;
+    int div = a / b;
+    int mod = a % b;
+
+    System.out.println("The sum of a and b is " + sum);
+    System.out.println("The sub of a and b is " + sub);
+    System.out.println("The mul of a and b is " + mul);
+    System.out.println("The div of a and b is " + div);
+    System.out.println("The mod of a and b is " + mod);
+  }
+}
---
+++ city.java
@@ -0,0 +1,9 @@
+public class city {
+
+  public static void main(String[] args) {
+    String city = "New York";
+    String state = "New York";
+    String country = "United States";
+    System.out.println(city + ", " + state + ", " + country);
+  }
+}
